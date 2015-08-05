fb445c842d7d0a2a56340b2e20e7ba5f40410683
alohageck0 <royalfiish@gmail.com>
2015-08-04 21:24:33 -0700

    08/04/15

--- Inheritance/src/Dog.java
+++ Inheritance/src/Dog.java
@@ -1,7 +1,11 @@
 
 public class Dog extends Animal {
+    private String name;
+    private String breed;
 
-    public Dog(String aType) {
+    public Dog(String aType, String aName, String aBreed) {
         super(aType);
+        name = aName;
+        breed = aBreed;
     }
 }
