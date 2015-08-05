644c7f68ccebd648cc5132f3ee4f4ace3c7db897
alohageck0 <royalfiish@gmail.com>
2015-08-04 21:19:01 -0700

    08/04/15

---
+++ Inheritance/src/Animal.java
@@ -0,0 +1,13 @@
+
+public class Animal {
+    private String type;
+
+    public Animal(String aType) {
+        type = aType;
+
+    }
+
+    public String getType() {
+        return type;
+    }
+}
