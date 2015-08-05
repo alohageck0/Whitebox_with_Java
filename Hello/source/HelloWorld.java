d4c29b89281db9c8779640441835f765aa7727ee
alohageck0 <royalfiish@gmail.com>
2015-08-04 17:03:14 -0700

    init

---
+++ Hello/source/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by royalfiish on 7/28/15.
+ * testing
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.print("Hello, World!");
+    }
+}
