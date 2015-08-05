d242f77254173ebbf6092317e29121e0ceb5cef2
alohageck0 <royalfiish@gmail.com>
2015-08-04 19:52:36 -0700

    08/04/15

--- Debug/src/Debug.java
+++ Debug/src/Debug.java
@@ -3,6 +3,17 @@
  */
 public class Debug {
     public static void main(String[] args) {
-        System.out.println("The result is");
+        int a = 20;
+        int b = 40;
+        boolean result = checkIsDouble(a, b);
+        System.out.println("The result is " + result);
     }
+
+    public static boolean checkIsDouble(int a, int b) {
+        if (a * 2 == b)
+            return true;
+        else
+            return false;
+    }
+
 }
