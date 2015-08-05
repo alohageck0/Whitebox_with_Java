fc496093732f545ba20217dbaeedea911cc5dcfd
alohageck0 <royalfiish@gmail.com>
2015-08-04 20:36:38 -0700

    08/04/15

--- MyModule/src/Point.java
+++ MyModule/src/Point.java
@@ -5,7 +5,12 @@ public class Point {
     private double x;
     private double y;
 
-    public static double distanceFromOrigin(String[] args) {
-        return double
+    public static double distanceFromOrigin() {
+        return Math.sqrt(x * x + y * y);
+    }
+
+    public Point(double xx, double yy) { //constructor
+        x = xx;
+        y = yy;
     }
 }
