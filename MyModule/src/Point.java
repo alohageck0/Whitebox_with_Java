93047a749675ba1b673d0d3991681b76aee55c39
alohageck0 <royalfiish@gmail.com>
2015-08-04 20:24:26 -0700

    08/04/15

---
+++ MyModule/src/Point.java
@@ -0,0 +1,11 @@
+/**
+ * Created by royalfiish on 8/4/15.
+ */
+public class Point {
+    private double x;
+    private double y;
+
+    public static double distanceFromOrigin(String[] args) {
+        return double
+    }
+}
