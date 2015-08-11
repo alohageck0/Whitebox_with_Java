b87c70882c2af1e610f79b1b761e88c42e158830
alohageck0 <royalfiish@gmail.com>
2015-08-10 18:11:34 -0700

    08/10/15

--- 08_09/src/Ractangle.java
+++ 08_09/src/Ractangle.java
@@ -2,20 +2,24 @@
 public class Ractangle extends Point {
     private int a;
     private int b;
-    private Point coords;
+    private Point coords1;
 
     public Ractangle(int aa, int bb, double xx, double yy) {
         super(xx, yy);
         a = aa;
         b = bb;
-        coords = new Point(xx, yy);
+        coords1 = new Point(xx, yy);
     }
 
     public double square() {
-        return a * a + b * b;
+        return a * b;
     }
 
-//    public boolean allcoords() {
-//
-//    }
+    public String allCoordsString() {
+        Point coord2 = new Point(coords1.x, coords1.y + b);
+        Point coord3 = new Point(coords1.x + a, coords1.y + b);
+        Point coord4 = new Point(coords1.x + a, coords1.y);
+        String coords = coords1.getCoords() + ',' + coord2.getCoords()+','+coord3.getCoords()+','+coord4.getCoords();
+        return coords;
+    }
 }
