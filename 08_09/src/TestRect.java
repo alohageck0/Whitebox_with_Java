5747304bed5df7de287ca64a660f41f2a29e165d
alohageck0 <royalfiish@gmail.com>
2015-08-10 18:13:52 -0700

    08/10/15

--- 08_09/src/TestRect.java
+++ 08_09/src/TestRect.java
@@ -1,6 +1,7 @@
 public class TestRect {
     public static void main(String[] args) {
-        Ractangle r = new Ractangle(2, 3, 1, 1);
+        Ractangle r = new Ractangle(3, 2, 1, 1);
         System.out.println(r.square());
+        System.out.println(r.allCoordsString());
     }
 }
