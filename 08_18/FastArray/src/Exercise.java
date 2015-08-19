b27dfbc80fda5d330cb9ac46a29ca2314adf3101
alohageck0 <royalfiish@gmail.com>
2015-08-18 20:31:11 -0700

    08/18/15

--- 08_18/FastArray/src/Exercise.java
+++ 08_18/FastArray/src/Exercise.java
@@ -5,11 +5,11 @@ public class Exercise {
         String name = "Java Java C++ Python Java Ruby C#";
         String[] langs = name.split(" ");
         HashMap<String, Integer> langsH = new HashMap<String, Integer>();
-        for (int i = 0; i < langs.length; i++) {
-            if (!langsH.containsKey(langs[i])) {
-                langsH.put(langs[i], 1);
+        for (String lang : langs) {
+            if (!langsH.containsKey(lang)) {
+                langsH.put(lang, 1);
             } else {
-                langsH.put(langs[i], langsH.get(langs[i]) + 1);
+                langsH.put(lang, langsH.get(lang) + 1);
             }
         }
         System.out.println(langsH);
