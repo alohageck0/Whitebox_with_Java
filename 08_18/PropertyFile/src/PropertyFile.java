7c101c44811d729aaf8e98bef32ed1373521b68e
alohageck0 <royalfiish@gmail.com>
2015-08-18 21:24:54 -0700

    08/18/15

--- 08_18/PropertyFile/src/PropertyFile.java
+++ 08_18/PropertyFile/src/PropertyFile.java
@@ -6,8 +6,8 @@ import java.util.Properties;
 public class PropertyFile {
     public static void main(String[] args) throws IOException {
         Properties properties = new Properties();
-        properties.load(new FileInputStream("/Users/royalfiish/IdeaProjects/Mango/08_18/pair.txt"));
-        String name = properties.getProperty("firstName");
+        properties.load(new FileInputStream("/Users/royalfiish/IdeaProjects/Mango/08_18/test"));
+        String name = properties.getProperty("first");
         System.out.println(name);
     }
 }
