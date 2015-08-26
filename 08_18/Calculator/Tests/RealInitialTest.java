7ef033daf897d1cffb7baf0a5b676139d9e5a4c3
alohageck0 <royalfiish@gmail.com>
2015-08-25 19:23:15 -0700

    08/25/15

--- 08_18/Calculator/Tests/RealInitialTest.java
+++ 08_18/Calculator/Tests/RealInitialTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class RealInitialTest {
-    private Calculator calculator;
+    public Calculator calculator;
 
     @Before
     public void setup() {
---
+++ 08_18/Calculator/Tests/Temlpate.java
@@ -0,0 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class Temlpate {
+    @Before
+    public void setUp() {
+        System.out.println("Before");
+    }
+
+    @Test
+    public void test1() {
+        System.out.println("Test1");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("Test2");
+    }
+}
