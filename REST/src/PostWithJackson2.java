2d52e858ef566332f493ae7283dcae0d9d31df21
alohageck0 <royalfiish@gmail.com>
2015-09-29 20:06:35 -0700

    09/29/15

---
+++ REST/src/PostWithJackson2.java
@@ -0,0 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class PostWithJackson2 {
+    private String service = "http://maps.googleapis.com";
+    private String resource = "/maps/api/geocode/{format}";
+    private String params = "?address={address}&sensor={sensor}";
+    private String url = service + resource + params;
+    private RestTemplate restTemplate;
+
+    @Before
+    public void setUp() {
+        restTemplate = new RestTemplate();
+    }
+
+    @Test
+    public void testPostWithJack() {
+        HashMap<String, String> vars = new HashMap<>();
+        vars.put("format", "json");
+        vars.put("address", "1600 Amphitheatre Parkway, Mountain View, CA");
+        vars.put("sensor", "false");
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        //HttpEntity<HashMap<String, String>> requestEntity = new HttpEntity<HashMap<String, String>>(vars, httpHeaders);
+        HttpEntity<String> requestEntity = new HttpEntity<>(httpHeaders);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class, vars);
+
+        MediaType responseHeader = response.getHeaders().getContentType();
+        System.out.println(responseHeader);
+    }
+}
